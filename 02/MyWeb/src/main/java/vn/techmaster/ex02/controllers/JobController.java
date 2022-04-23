fa7884cfd0917559980f58009035a7e1b6b81bf8
longpanhpanh <tranthanhlongita@gmail.com>
2022-04-23 21:14:08 +0700

    Typo fix

--- 02/MyWeb/src/main/java/vn/techmaster/ex02/controllers/JobController.java
+++ 02/MyWeb/src/main/java/vn/techmaster/ex02/controllers/JobController.java
@@ -2,7 +2,7 @@ package vn.techmaster.ex02.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import vn.techmaster.ex02.model.Job;
-import vn.techmaster.ex02.services.JobServiceImpl;
+import vn.techmaster.ex02.services.JobService;
 
 import java.util.List;
 import java.util.Set;
@@ -10,58 +10,58 @@ import java.util.Set;
 @RestController
 @RequestMapping("/job")
 public class JobController {
-    private final JobServiceImpl jobServiceImpl;
+    private final JobService jobService;
 
-    public JobController(JobServiceImpl jobServiceImpl) {
-        this.jobServiceImpl = jobServiceImpl;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
     }
 
 
     @GetMapping
     public Set<Job> getJobs() {
-        return jobServiceImpl.findAll();
+        return jobService.findAll();
     }
 
     @GetMapping("/{id}")
     public Job getJobById(@PathVariable("id") String id) {
-        return jobServiceImpl.findById(id);
+        return jobService.findById(id);
     }
 
 
     @GetMapping("/sortbylocation")
     public List<Job> getSortedJobs() {
-        return jobServiceImpl.sortByLocation();
+        return jobService.sortByLocation();
     }
 
     @GetMapping("/salary/{salary}")
     public Set<Job> getJobsBySalary(@PathVariable("salary") int salary) {
-        return jobServiceImpl.getJobBySalary(salary);
+        return jobService.getJobBySalary(salary);
     }
 
     @GetMapping("/keyword/{keyword}")
     public Set<Job> getJobsByKeyword(@PathVariable("keyword") String keyword) {
-        return jobServiceImpl.getJobByKeyword(keyword);
+        return jobService.getJobByKeyword(keyword);
     }
 
     @GetMapping("/query")
     public Set<Job> getJobsByLocationAndKeyword(@RequestParam("location") String location,
                                                 @RequestParam("keyword") String keyword) {
-        return jobServiceImpl.getJobByLocationAndKeyWord(location, keyword);
+        return jobService.getJobByLocationAndKeyWord(location, keyword);
     }
 
     @PostMapping
     public Job addJob(@RequestBody Job job) {
-        return jobServiceImpl.addJob(job);
+        return jobService.addJob(job);
     }
 
     @PutMapping("/{id}")
     public Job updateJob(@PathVariable("id") String id, @RequestBody Job job) {
-        return jobServiceImpl.updateById(id, job);
+        return jobService.updateById(id, job);
     }
 
     @DeleteMapping("/{id}")
     public Job deleteJob(@PathVariable("id") String id) {
-        return jobServiceImpl.deleteById(id);
+        return jobService.deleteById(id);
     }
 
 
