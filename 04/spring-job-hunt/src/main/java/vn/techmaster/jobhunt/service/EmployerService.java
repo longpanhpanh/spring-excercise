4d631d6d580547f53d762a9b54046a365bf22b5d
longpanhpanh <tranthanhlongita@gmail.com>
2022-05-24 16:24:05 +0700

    add edit/delete employer

---
+++ 04/spring-job-hunt/src/main/java/vn/techmaster/jobhunt/service/EmployerService.java
@@ -0,0 +1,17 @@
+package vn.techmaster.jobhunt.service;
+
+import java.util.List;
+
+import vn.techmaster.jobhunt.dto.EmployerRequest;
+import vn.techmaster.jobhunt.model.Employer;
+
+public interface EmployerService {
+    List<Employer> getAllEmployers();
+    Employer getEmployerById(String employerId);
+    List<Employer> getEmployersByName(String employerName);
+
+    Employer addEmployer(EmployerRequest employerRequest);
+    Employer updateEmployer(Employer employer);
+    void delete(String employerId);
+
+}
